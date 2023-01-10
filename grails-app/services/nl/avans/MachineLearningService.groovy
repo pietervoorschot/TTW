38b8fe607b5b0712000613f34d5052c3c8f61a85
cvand <c.vandersteen3@gmail.com>
2023-01-10 16:31:14 +0100

    ML service WIP 2

--- grails-app/services/nl/avans/MachineLearningService.groovy
+++ grails-app/services/nl/avans/MachineLearningService.groovy
@@ -39,6 +39,48 @@ class MachineLearningService {
         rootNode = new BinTree(newNodeID, newQuestAns);
         System.out.println("Created root node "+newNodeID);
     }
+    //ADD YES NODE
+    public void addYesNode(int existingNodeID, int newNodeID, String newQuestAns){
+        //If no root node does nothing
+        if(rootNode ==null){
+            System.out.println("ERROR: geen root node!");
+            return;
+        }
 
+        //search tree
+
+        if (searchTreeAndAddYesNode(rootNode, existingNodeID, newNodeID, newQuestAns)){
+            System.out.println("Added node "+ newNodeID +
+            " onto " \ yes\ "branch of node " + existingNodeID);
+        }
+        else System.out.println("Node " + existingNodeID + " not found");
+    }
+
+    //SEARCH TREE AND ADD YES NODE
+    private boolean searchTreeAndAddYesNode(BinTree currentNode, int existingNodeID, int newNodeID, String newQuestAns){
+        if (currentNode.nodeID == existingNodeID){
+            // Found node
+            if (currentNode.yesBranch ==null) currentNode.yesBranch = new BinTree(newNodeID, newQuestAns);
+            else {
+                System.out.println("WARNING: vorige node aan het overschrijven "+ "(id = " + currentNode.yesBranch.nodeID + ") linked to yes branch of node " + existingNodeID);
+                currentNode.yesBranch = new BinTree(newNodeID, newQuestAns);
+            }
+            return(true);
+        }
+        else{
+            //try yes branch if it exists
+            if (currentNode.yesBranch !=null){
+                if (searchTreeAndAddYesNode(currentNode.noBranch, existingNodeID, newNodeID, newQuestAns)){
+                    return(true);
+                }
+                else{
+                    //try no branch if it exists
+                    if (currentNode.noBranch != null){
+                        return(searchTreeAndAddYesNode(currentNode.noBranch, existingNodeID, newNodeID,newQuestAns));
+                    }
+                }
+            }
+        }
+    }
 
 }
